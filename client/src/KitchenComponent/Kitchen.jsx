fa620a63eb90ccc9d376de3768248da4801f1b1b
newbz521 <lawrence.yee1991@gmail.com>
2023-04-01 14:56:11 -0400

    kitchen update

--- client/src/KitchenComponent/Kitchen.jsx
+++ client/src/KitchenComponent/Kitchen.jsx
@@ -3,6 +3,9 @@ import { useEffect, useState, useRef } from "react";
 import { Canvas, useFrame, useThree, PerspectiveCamera} from "@react-three/fiber"
 import Chrome from "./materials/chrome.jpeg"
 import WengeVertical from "./materials/WengeVertical.jpeg"
+import Wenge from "./materials/Wenge.jpeg"
+import Marble from "./materials/streaked-marble.png"
+import lightChrome from "./materials/lightChrome.jpeg"
 import { Mesh, TextureLoader } from "three"
 import Spotlight from "./spotlight";
 import {Stats, OrbitControls} from "@react-three/drei"
@@ -14,8 +17,9 @@ const KitchenScene = (props) => {
   const [kitchenDepth, setKitchenDepth] = useState(9);
   const [cabinets, setCabinets] = useState([3, 3, 3]);
   const [cabinetMaterial, setCabinetMaterial] = useState(WengeVertical)
-  // const [tableMaterial, setTableMaterial] = useState(Marble)
-  // const [tableBottomMaterial,setTableBottomMaterial] = useState(Wenge)
+  const [tableMaterial, setTableMaterial] = useState(Marble)
+  const [tableBottomMaterial, setTableBottomMaterial] = useState(Wenge)
+  const [stoveMaterial, setStoveMaterial] = useState(lightChrome)
   const [toggle, setToggle] = useState(true)
   const [wired, setWired] = useState(false)
   const [staticIso, setStaticIso] = useState(false)
@@ -23,6 +27,10 @@ const KitchenScene = (props) => {
   const loader = new TextureLoader();
   const chromeTexture = loader.load(Chrome)
   const verticalWoodTexture = loader.load(cabinetMaterial)
+  const marbleTexture = loader.load(tableMaterial)
+  const woodTexture = loader.load(tableBottomMaterial)
+  const stoveTexture = loader.load(stoveMaterial)
+  let bottomLength =  (kitchenLength - 2) - 1.5/12
   
   function divideLength(length, minSectionLength, maxSectionLength) {
     const numSections = Math.ceil(length / maxSectionLength);
@@ -185,6 +193,118 @@ const KitchenScene = (props) => {
     )
   }
 
+  function CounterTop() {
+    const oneRef = useRef(null)
+    const twoRef = useRef(null)
+    useFrame(() => { 
+      oneRef.current.position.z = -(kitchenDepth / 2) + 1.1;
+      oneRef.current.position.y = 3 + 1.5 / 24
+      oneRef.current.receiveShadow = true;
+      oneRef.current.castShadow = true;
+
+      twoRef.current.position.z = -(kitchenDepth / 2) + 1.1;
+      twoRef.current.position.y = 1.5
+      twoRef.current.receiveShadow = true;
+      twoRef.current.castShadow = true;
+    })
+    return (
+      <>
+      <group ref={oneRef} >
+        <mesh> 
+          <boxGeometry args={[kitchenLength, 1.5/12, 2.2]} /> 
+          <meshBasicMaterial map={marbleTexture} />
+        </mesh>
+        </group>
+        <group ref={twoRef} >
+        <mesh> 
+          <boxGeometry args={[ kitchenLength, 3, 2 ]} /> 
+          <meshBasicMaterial map={woodTexture} />
+        </mesh>
+        </group>
+      </>
+    )
+  }
+  function CounterTopTwo() {
+    const oneRef = useRef(null)
+    const twoRef = useRef(null)
+    useFrame(() => { 
+      oneRef.current.position.y = 3 + 1.5/24;
+      oneRef.current.position.z = 0;
+      oneRef.current.position.x = (kitchenLength / 2) - 1.1;
+      oneRef.current.receiveShadow = true;
+      oneRef.current.castShadow = true;
+
+      twoRef.current.position.y = 1.5
+      twoRef.current.position.z = 0;
+      twoRef.current.position.x = (kitchenLength / 2) - 1;
+      twoRef.current.receiveShadow = true;
+      twoRef.current.castShadow = true;
+    })
+    return (
+      <>
+      <group ref={oneRef} >
+        <mesh> 
+          <boxGeometry args={[2.2, 1.5/12, kitchenDepth]} /> 
+          <meshBasicMaterial map={marbleTexture} />
+        </mesh>
+      </group>
+              <group ref={twoRef} >
+              <mesh> 
+                <boxGeometry args={[2, 3, kitchenDepth ]} /> 
+                <meshBasicMaterial map={woodTexture} />
+              </mesh>
+        </group>
+        </>
+    )
+  }
+  function CounterTopThree() {
+    const oneRef = useRef(null)
+    const twoRef = useRef(null)
+    const threeRef = useRef(null)
+    useFrame(() => { 
+      oneRef.current.position.x = kitchenLength/2 - (kitchenLength/2 + 1)
+      oneRef.current.position.y = 3 + 1.5/24
+      oneRef.current.position.z = kitchenDepth/2 - kitchenDepth/6 
+      oneRef.current.flatShading = true;
+      oneRef.current.receiveShadow = true;
+      oneRef.current.castShadow = true;
+
+      twoRef.current.position.x = -kitchenLength/2  + 1.5/24
+      twoRef.current.position.y = 1.5
+      twoRef.current.position.z = kitchenDepth/2 - kitchenDepth/6 
+      twoRef.current.receiveShadow = true;
+      twoRef.current.castShadow = true;
+
+      threeRef.current.position.x = kitchenLength/2 - (bottomLength/2 + 2)
+      threeRef.current.position.y = 1.5
+      threeRef.current.position.z = kitchenDepth / 2 - kitchenDepth / 6 - .5
+      threeRef.current.receiveShadow = true;
+      threeRef.current.castShadow = true;
+    })
+    return (
+      <>
+      <group ref={oneRef} >
+        <mesh> 
+          <boxGeometry args={[ (kitchenLength - 2), 1.5/12, kitchenDepth /3 ]} /> 
+          <meshBasicMaterial map={marbleTexture} />
+        </mesh>
+      </group>
+        <group ref={twoRef} >
+        <mesh> 
+          <boxGeometry args={[1.5/12 , 3, kitchenDepth /3 ]} /> 
+          <meshBasicMaterial map={marbleTexture} />
+        </mesh>
+        </group>
+        <group ref={threeRef} >
+        <mesh> 
+          <boxGeometry args={[bottomLength, 3, kitchenDepth /3 - 1 ]} /> 
+          <meshBasicMaterial map={woodTexture} />
+        </mesh>
+        </group>
+      </>
+    )
+}
+
 
   function CabinetEach({position,data, positionLeftDoor}) {
     
@@ -206,8 +326,6 @@ const KitchenScene = (props) => {
     )
     }
 
-
-  
       const Lights = (positions) => {
         const lightRef = useRef()
         const targetRef = useRef()
@@ -250,7 +368,48 @@ const KitchenScene = (props) => {
           </>
         )
       }
+  
+  function Appliances() {
+    const oneRef = useRef(null)
+    const twoRef = useRef(null)
+    const threeRef = useRef(null)
+
+    useFrame(() => {
+      oneRef.current.position.x =  kitchenLength/2 - (bottomLength/2 + 2) 
+      oneRef.current.position.z = kitchenDepth / 2 - kitchenDepth
+        / 6 - .75 
+      oneRef.current.position.y = 6
+      oneRef.current.receiveShadow = true
+      oneRef.current.castShadow = true
+
+      twoRef.current.position.x =  kitchenLength/2 - (bottomLength/2 + 2) 
+      twoRef.current.position.z = kitchenDepth / 2 - kitchenDepth / 6 - .75 
+      twoRef.current.position.y = 7.2
+      twoRef.current.receiveShadow = true
+      twoRef.current.castShadow = true
+
+      threeRef.current.position.x =  kitchenLength/2 - (bottomLength/2 + 2) 
+      threeRef.current.position.z = kitchenDepth / 2 - kitchenDepth / 6 - .75 
+      threeRef.current.position.y = 3 + 3/24
+    })
+    return (
+      <>
+      <mesh ref={oneRef}>
+        <boxGeometry args={[5, 9 / 12, 1.5]} />
+        <meshMatcapMaterial matcap={stoveTexture} />
+        </mesh>
+      <mesh ref={twoRef}>
+        <cylinderGeometry args={[.5, 8 / 12, 2]} />
+        <meshMatcapMaterial matcap={stoveTexture} />
+        </mesh>
+      <mesh ref={threeRef}>
+        <boxGeometry args={[5, 1.5 / 12, 1.5]} />
+        <meshMatcapMaterial matcap={chromeTexture} />
+      </mesh>
       
+      </>
+    )
+  }
   return (
     <div className="canvasContainer">
     <Canvas  camera={{ position: [cam, 5.5, 15] }}>
@@ -266,19 +425,17 @@ const KitchenScene = (props) => {
       {cabinets.map((data, index) => 
         (<CabinetEach data={data} position={[(-kitchenLength / 2 + data / 2) + (index * data), 4.5 + (2), -(kitchenDepth / 2) + .5]} positionLeftDoor={[(-kitchenLength / 2 + data / 2) + (index * data),4.5 + (2),-(kitchenDepth / 2) + 1.07]} />)
       )}
-      {/* {cabinets.map((data, index) => 
-        (<LightRowOne index={index} data={data} position={[(-kitchenLength / 2 + data / 2) + (index * data), 15, -kitchenDepth/2 + 4]} />)
-        )}
-      {cabinets.map((data, index) => 
-        (<LightRowTwo index={index} data={data} position={[(-kitchenLength / 2 + data / 2) + (index * data), 15, kitchenDepth/2 + 6 ]} />)
-        )} */}
+  
         {cabinets.map((data, index) => 
           (<Lights number={(-kitchenLength / 2 + data / 2) + (index * data)} />)
         )}
         {cabinets.map((data, index) => 
           (<LightsTwo number={(-kitchenLength / 2 + data / 2) + (index * data)} />)
       )}
-  
+        <CounterTop />
+        <CounterTopTwo />
+        <CounterTopThree />
+        <Appliances/>
     </Canvas>
 
       <div className="edit-container">
